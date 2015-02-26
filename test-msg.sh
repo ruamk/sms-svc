31cf2ccea78cc3b62f48979cd0951f7b25a8e10b
Max Taldykin <jorpic@gmail.com>
2015-02-26 09:39:01 +0300

    Add test-msg.sh

---
+++ test-msg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+PHONE=$1
+API="http://en.wikipedia.org/w/api.php\
+?action=query\
+&prop=extracts\
+&format=json\
+&exsentences=2\
+&explaintext=\
+&exsectionformat=plain\
+&rawcontinue=\
+&generator=random\
+&grnnamespace=0"
+MSG=`curl -s "$API" | jq -r '.query.pages | to_entries[] | .value.extract'`
+echo $MSG
+psql carma -c "insert into \"Sms\" (status, phone, msgtext) values \
+  ('please-send', '$PHONE', \$XXX\$$MSG\$XXX\$)"
